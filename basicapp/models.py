d766271a06a7bcb979cae4fc7f6f51e10492cef9
akatsuki06 <suraznegi@gmail.com>
2017-12-27 19:14:28 +0530

    added model

--- basicapp/models.py
+++ basicapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+from django.utils import timezone
 # Create your models here.
+class Link(models.Model):
+    label=models.CharField(max_length=50,blank=True)
+    created_date=models.DateTimeField(blank=True, default=timezone.now)
+    shortURL=models.CharField(max_length=10,primary_key=True)
