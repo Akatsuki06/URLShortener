5f1dad5a85769606c7515df9087bd8ad3b72e731
akatsuki06 <suraznegi@gmail.com>
2017-12-27 19:08:27 +0530

    added files

--- URLShortener/settings.py
+++ URLShortener/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'basicapp'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR,'static')
---
+++ basicapp/urls.py
@@ -0,0 +1,1 @@
+from djano.urls import path
